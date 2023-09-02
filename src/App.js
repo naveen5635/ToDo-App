fabee4c21e73b6de8b7791aab4d2114033824635
Naveen Joy <53271107+naveen5635@users.noreply.github.com>
2023-09-02 19:17:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './footer'
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper  />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    <p>Made with ❤️ by </p>
+    <a href="https://github.com/naveen5635">Naveen Joy</a>
+  </div>
+);
+
+export default Footer;
